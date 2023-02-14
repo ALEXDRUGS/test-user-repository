b6ddef21aeb0a9f3c2d17e1da1c026225a8ead40
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-02-14 15:29:41 +0300

    SQL

---
+++ src/main/java/.sql
@@ -0,0 +1,39 @@
+SELECT *
+FROM employee;
+
+SELECT first_name AS имя,
+       last_name  AS фамилия
+FROM employee;
+
+SELECT first_name, last_name, gender, age
+FROM employee
+WHERE age < 30
+   OR age > 50;
+
+SELECT first_name, last_name, gender, age
+FROM employee
+WHERE age > 30
+  AND age < 50;
+
+SELECT *
+FROM employee
+ORDER BY last_name DESC;
+
+SELECT *
+FROM employee
+WHERE length(first_name) > 4;
+
+UPDATE employee
+SET first_name='Kurt'
+WHERE id = 7;
+
+UPDATE employee
+SET first_name='Monica'
+WHERE id = 2;
+
+SELECT first_name AS Имя,
+       sum(age)   AS Суммарный_возраст
+FROM employee
+WHERE first_name = 'Kurt'
+   OR first_name = 'Monica'
+GROUP BY first_name, age;

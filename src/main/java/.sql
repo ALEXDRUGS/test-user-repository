d9f34642cd78b94564e78862d2a3fb80d2d6a8ba
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-02-15 09:43:31 +0300

    SQLfin

--- src/main/java/.sql
+++ src/main/java/.sql
@@ -24,28 +24,27 @@ FROM employee
 WHERE length(first_name) > 4;
 
 UPDATE employee
-SET first_name='Kurt'
+SET first_name = 'Kurt'
 WHERE id = 7;
 
 UPDATE employee
-SET first_name='Monica'
+SET first_name = 'Monica'
 WHERE id = 2;
 
 SELECT first_name AS Имя,
        sum(age)   AS Суммарный_возраст
 FROM employee
-WHERE first_name = 'Kurt'
-   OR first_name = 'Monica'
+GROUP BY Имя
+HAVING count(first_name) > 1;
+
+SELECT first_name AS Имя,
+       min(age)   AS Минимальный_возраст
+FROM employee
 GROUP BY first_name;
 
 SELECT first_name AS Имя,
-       min(age) AS Минимальный_возраст
+       max(age)   AS Максимальный_возраст
 FROM employee
-GROUP BY first_name;
-
-SELECT max(age)   AS Максимальный_возраст,
-       first_name AS Имя
-FROM employee
-WHERE first_name = 'Kurt'
-   OR first_name = 'Monica'
-group by first_name;
+GROUP BY Имя
+HAVING count(first_name) > 1
+ORDER BY Максимальный_возраст;
